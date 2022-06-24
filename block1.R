58a65b46650bb6ff8488692033b4a2be39a103d6
George Perrett <georgeperrett@10-17-50-78.dynapool.wireless.nyu.edu>
2022-06-24 16:55:46 -0400

    chnaged results formation

--- block1.R
+++ block1.R
@@ -8,13 +8,15 @@ source('get_balance.R')
 source('models.R')
 source('observational_random_intercepts.R')
 
-iteration <- Sys.getenv('SLURM_ARRAY_TASK_ID')
+get.seed <- Sys.getenv('SLURM_ARRAY_TASK_ID')
 
 taus <- c(0, .1, .2, .5, .8)
 
-results <- map(taus, function(i){
-  observational_random_intercepts(tau = taus[i], seed = iteration)
-})
+A <- lapply(1:length(taus), function(i) observational_random_intercepts(tau = taus[i], type = 'A', seed = get.seed))
+B <- lapply(1:length(taus), function(i) observational_random_intercepts(tau = taus[i], type = 'B', seed = get.seed))
+C <- lapply(1:length(taus), function(i) observational_random_intercepts(tau = taus[i], type = 'C', seed = get.seed))
+
+results <- loo::nlist(A, B, C)
 
 out_file <- paste0('iteration_', iteration, '.csv')
 write_rds(results, out_file)
