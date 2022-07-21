6d70f8b3affd07bdf2f1de86fafb6e2868459cac
George Perrett <49445400+gperrett@users.noreply.github.com>
2022-07-21 12:33:29 -0400

    Update load_ihdp.R

--- load_ihdp.R
+++ load_ihdp.R
@@ -1,18 +1,19 @@
-load_ihdp <- function(){
+load_ihdp <- function(rct = FALSE){
   dat <- read_csv('data/ihdp.csv')
   dat <- subset(dat, treat != 1 | momwhite != 0)
-  # truncate momage to group 1
-    dat <- dat %>% 
-    mutate(g1 = as.factor(if_else(momage < 15 , 15, momage)), 
-           g1 = as.factor(if_else(momage > 40, 40 , momage)), 
-           g2 = as.factor(site.num)) %>% 
-    select(-momage)
+  dat$momage[dat$momage < 15] <- 15
+  dat$momage[dat$momage > 40] <- 40
+  dat$g1 <- as.factor(dat$momage)
+  dat$g2 <- as.factor(dat$site.num)  
     
-    covs.cont <- c("bw", "b.head", "preterm", "birth.o", "nnhealth")
-    covs.cat  <- c("sex", "twin", "b.marr", "mom.lths", "mom.hs", "mom.scoll",
+  covs.cont <- c("bw", "b.head", "preterm", "birth.o", "nnhealth")
+  covs.cat  <- c("sex", "twin", "b.marr", "mom.lths", "mom.hs", "mom.scoll",
                    "cig", "first", "booze", "drugs", "work.dur", "prenatal")
+  
+  set.seed(2)
+  if(isTRUE(rct)) ihdp$treat <- rbinom(nrow(dat), 1, .5)
     
-    dat <- dat[, c(covs.cat, covs.cont, 'treat', 'g1', 'g2')]
+  dat <- dat[, c(covs.cat, covs.cont, 'treat', 'g1', 'g2', 'momage')]
   
   
   return(dat)  
