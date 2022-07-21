410296993cfe5a70a45336e1c6ae8f9d4869f4a1
George Perrett <49445400+gperrett@users.noreply.github.com>
2022-07-21 12:51:44 -0400

    Update models.R

--- models.R
+++ models.R
@@ -55,18 +55,17 @@ extract.lme4 <- function(fit){
 }
 
 extract.bart <- function(fit, .model){
-  y.pred <- apply(bartCause::extract(fit, "mu.obs"), 2, mean)
-  estimates <- summary(fit)$estimates
+  estimates <- summary(fit, target = 'sate')$estimates
   average.pred <- estimates$estimate
   interval <- c(estimates$ci.lower, estimates$ci.upper)
   samples.icate <- bartCause::extract(fit, 'icate')
   icate.pred <- apply(samples.icate, 2, mean)
   
-  g1.pred <- unlist(lapply(split(samples.icate, dat$g1[dat$treat == 1]), mean))
-  g1.pred <- g1.pred[!is.na(g1.pred)]
-  
-  g2.pred <- unlist(lapply(split(samples.icate, dat$g2[dat$treat == 1]), mean))
-  g2.pred <- g2.pred[!is.na(g2.pred)]
+  # get group level predictions 
+  g1 <- dat$g1[dat$treat == 1]
+  g2 <- dat$g2[dat$treat == 1]
+  g1.pred <- unlist(lapply(split(icate.pred, g1), mean))
+  g2.pred <- unlist(lapply(split(icate.pred, g2), mean))
   
   out <- data.frame(
     u_bias = average.pred - average.truth,
@@ -80,35 +79,81 @@ extract.bart <- function(fit, .model){
     tau = tau
   )
   
+  
   return(out)
 }
 
+interval.extract.bart <- function(fit, .model){
+  samples.icate <- bartCause::extract(fit, 'icate')
+  icate.pred <- apply(samples.icate, 2, mean)
+  
+  # get group level predictions 
+  g1 <- dat$g1[dat$treat == 1]
+  g2 <- dat$g2[dat$treat == 1]
+  g1.pred <- unlist(lapply(split(icate.pred, g1), mean))
+  g2.pred <- unlist(lapply(split(icate.pred, g2), mean))
+  
+  g1.splits <- split(as.data.frame(t(samples.icate)), g1)
+  g1.splits <- lapply(names(g1.splits), function(i) apply(g1.splits[[i]], 2, mean))
+  g1.intervals <- lapply(1:length(g1.splits), function(i)quantile(g1.splits[[i]], c(.025, .975)))
+  g1.intervals <- bind_rows(g1.intervals)
+  g1.intervals <- cbind(g1.intervals,g1.pred ,g1.truth)
+  g1.intervals$u_bias <- g1.pred - g1.truth 
+  g1.intervals$s_bias <- (g1.pred - g1.truth)/sd(y)
+  g1.intervals$int.length <- g1.intervals$`97.5%` - g1.intervals$`2.5%`
+  g1.intervals$model <- .model
+  g1.intervals$group <- rownames(g1.intervals)
+  g1.intervals$tau <- tau
+  
+  g2.splits <- split(as.data.frame(t(samples.icate)), g2)
+  g2.splits <- lapply(names(g2.splits), function(i) apply(g2.splits[[i]], 2, mean))
+  g2.intervals <- lapply(1:length(g2.splits), function(i)quantile(g2.splits[[i]], c(.025, .975)))
+  g2.intervals <- bind_rows(g2.intervals)
+  g2.intervals <- cbind(g2.intervals,g2.pred ,g2.truth)
+  g2.intervals$u_bias <- g2.pred - g2.truth 
+  g2.intervals$s_bias <- (g2.pred - g2.truth)/sd(y)
+  g2.intervals$int.length <- g2.intervals$`97.5%` - g2.intervals$`2.5%`
+  g2.intervals$model <- .model
+  g2.intervals$group <- rownames(g2.intervals)
+  g2.intervals$tau <- tau
+  
+  list(g1.intervals, g2.intervals)
+}
+
+
+
 
 extract.stan4bart <- function(fit, .model){
-  # patt
-  samples.ppd.train <- stan4bart:::extract.stan4bartFit(fit, type = "ppd", sample = "train")
-  samples.ppd.test <- stan4bart:::extract.stan4bartFit(fit, type = "ppd", sample = "test")
-  samples.patt <- apply((samples.ppd.train[dat$treat ==1,] - samples.ppd.test[dat$treat ==1,]) * (2 * fit$frame[[fit$treatment]][dat$treat ==1] - 1), 2, mean)
+
+  samples.ppd.test <- extract(fit, type = "ppd", sample = "test")
+  
+  # Individual sample treatment effects
+  samples.ite <- (dat$y - samples.ppd.test) * (2 * dat$treat - 1)
+  samples.ite <- samples.ite[dat$treat == 1,]
+
+    # Sample average treatment effect
+  samples.sate <- apply(samples.ite, 2, mean)
+  sate <- mean(samples.sate)
+  sate.int <- quantile(samples.sate, c(.025, .975))
   
   # icate
   samples.mu.train <- stan4bart:::extract.stan4bartFit(fit)
   samples.mu.test  <- stan4bart:::extract.stan4bartFit(fit, sample = "test")
-  samples.icate <- (samples.mu.train[dat$treat ==1,]  - samples.mu.test[dat$treat ==1,]) * (2 * fit$frame[[fit$treatment]][dat$treat ==1] - 1)
+  samples.icate <- (samples.mu.train  - samples.mu.test) * (2 * fit$frame[[fit$treatment]] - 1)
   
   # results
-  y.pred <- apply(samples.ppd.train, 1, mean)
-  average.pred <- mean(samples.patt)
-  interval <- c(average.pred - 1.96 * sd(samples.patt), average.pred + 1.96 * sd(samples.patt))
+  average.pred <- sate
+  interval <- sate.int
   icate.pred <- apply(samples.icate, 1, mean)
+  icate.pred <- icate.pred[dat$treat == 1]
   
-  g1 <- dat$g1[dat$treat ==1]
-  g2 <- dat$g2[dat$treat ==1]
+  # get group level predictions 
+  g1 <- dat$g1[dat$treat == 1]
+  g1.pred <- unlist(lapply(split(icate.pred, g1), mean))
   
-  g1.pred <- unlist(lapply(split(samples.icate, g1), mean))
-  g1.pred <- g1.pred[!is.na(g1.pred)]
+  g2 <- dat$g2[dat$treat == 1]
+  g2.pred <- unlist(lapply(split(icate.pred, g2), mean))
   
-  g2.pred <- unlist(lapply(split(samples.icate, g2), mean))
-  g2.pred <- g2.pred[!is.na(g2.pred)]
   
   out <- data.frame(
     u_bias = average.pred - average.truth,
@@ -125,6 +170,59 @@ extract.stan4bart <- function(fit, .model){
   return(out)
   
 }
+
+interval.extract.stan <- function(fit, .model){
+  samples.ppd.test <- extract(fit, type = "ppd", sample = "test")
+  
+  # Individual sample treatment effects
+  samples.ite <- (dat$y - samples.ppd.test) * (2 * dat$treat - 1)
+  samples.ite <- samples.ite[dat$treat == 1,]
+  
+  # Sample average treatment effect
+  samples.sate <- apply(samples.ite, 2, mean)
+  sate <- mean(samples.sate)
+  sate.int <- quantile(samples.sate, c(.025, .975))
+  
+  
+  # get group level predictions 
+  g1 <- dat$g1[dat$treat == 1]
+
+  g2 <- dat$g2[dat$treat == 1]
+
+  # get group level intervals 
+  g1.splits <- split(as.data.frame(samples.ite), g1)
+  g1.splits <- lapply(names(g1.splits), function(i) apply(g1.splits[[i]], 2, mean))
+  g1.pred <- unlist(lapply(g1.splits, mean))
+  g1.intervals <- lapply(1:length(g1.splits), function(i)quantile(g1.splits[[i]], c(.025, .975)))
+  g1.intervals <- bind_rows(g1.intervals)
+  g1.intervals <- cbind(g1.intervals,g1.pred ,g1.truth)
+  g1.intervals$u_bias <- g1.pred - g1.truth 
+  g1.intervals$s_bias <- (g1.pred - g1.truth)/sd(y)
+  g1.intervals$int.length <- g1.intervals$`97.5%` - g1.intervals$`2.5%`
+  g1.intervals$model <- .model
+  g1.intervals$group <- unique(g1)
+  g1.intervals$tau <- tau
+  
+  
+  g2.splits <- split(as.data.frame(samples.ite), g2)
+  g2.splits <- lapply(names(g2.splits), function(i) apply(g2.splits[[i]], 2, mean))
+  g2.pred <- unlist(lapply(g2.splits, mean))
+  g2.intervals <- lapply(1:length(g2.splits), function(i)quantile(g2.splits[[i]], c(.025, .975)))
+  g2.intervals <- bind_rows(g2.intervals)
+  g2.intervals <- cbind(g2.intervals,g2.pred ,g2.truth)
+  g2.intervals$u_bias <- g2.pred - g2.truth 
+  g2.intervals$s_bias <- (g2.pred - g2.truth)/sd(y)
+  g2.intervals$int.length <- g2.intervals$`97.5%` - g2.intervals$`2.5%`
+  g2.intervals$model <- .model
+  g2.intervals$group <- unique(g2)
+  g2.intervals$tau <- tau
+  
+  list(g1.intervals, g2.intervals)
+}
+
+
+
+
 
 
 
